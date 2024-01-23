e3eca00dff52befe281d69698f7caa379abb3049
ariefraihandi <raihandi93@gmail.com>
2024-01-23 23:20:40 +0700

    Update

--- resources/views/Konten/AuthRegister.blade.php
+++ resources/views/Konten/AuthRegister.blade.php
@@ -75,7 +75,7 @@
 
           <p class="text-center">
             <span>Already have an account?</span>
-            <a href="{{ route('register') }}">
+            <a href="{{ route('login') }}">
               <span>Sign in instead</span>
             </a>
           </p>
